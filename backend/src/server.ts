d23e975e3db620ae9c4fb9b9c389383aa68f3a22
Abhichidrala <108573773+Abhichidrala@users.noreply.github.com>
2026-05-17 12:38:44 +0530

    fixed cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,11 +9,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);app.use(express.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 app.use("/api/auth", authRoutes);
